6223d8705672b6855bc41ba8a9c2bf6e5cf6d95e
fahad <fahadmapari09@gmail.com>
2020-10-09 23:19:05 +0530

    half way there

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+
+function university_files()
+{
+
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    if (strstr($_SERVER['SERVER_NAME'], 'university.loca')) {
+        wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), '1.0', true);
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
+    }
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features()
+{
+    add_theme_support('title-tag');
+    register_nav_menu('headerMainMenu', 'Header Main Menu');
+}
+
+add_action('after_setup_theme', 'university_features');
+
+
+function university_adjust_queries($query)
+{
+    if (!is_admin() and is_post_type_archive('program') and is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
+    if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => date('Ymd'),
+            'type' => 'numeric'
+        ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
